master-hax/aosp-nfc

Add logical connections management
Bug: 197333653
Test: build ok
Change-Id: I84f972978fa50b8611819b6454313fef23fd7b6a

src/rust/nci/nci.rs
@@ -16,15 +16,20 @@
 //! Supports sending NCI commands to the HAL and receiving
 //! NCI messages back
 
+use bytes::{BufMut, BytesMut};
 use log::{debug, error};
 use nfc_hal::{Hal, HalEventRegistry};
+use nfc_packets::nci::DataChild::Payload;
 use nfc_packets::nci::NciChild::{Notification, Response};
-use nfc_packets::nci::{CommandPacket, DataPacket, NotificationPacket, Opcode, ResponsePacket};
+use nfc_packets::nci::NotificationChild::ConnCreditsNotification;
+use nfc_packets::nci::{CommandPacket, DataBuilder, DataPacket};
+use nfc_packets::nci::{NotificationPacket, Opcode, Packet, PacketBoundaryFlag, ResponsePacket};
 use std::collections::HashMap;
-use std::sync::Arc;
+use std::collections::VecDeque;
+use std::sync::{Arc, Mutex};
 use tokio::select;
-use tokio::sync::mpsc::{channel, Receiver, Sender};
-use tokio::sync::{oneshot, Mutex};
+use tokio::sync::mpsc::{channel, Receiver, Sender, UnboundedSender};
+use tokio::sync::{oneshot, RwLock};
 use tokio::time::{sleep, Duration, Instant};
 
 pub mod api;
@@ -35,21 +40,23 @@ type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>
 /// Initialize the module and connect the channels
 pub async fn init() -> Nci {
     let hc = nfc_hal::init().await;
-    // Channel to handle data downstream messages
-    let (out_data_ext, out_data_int) = channel::<DataPacket>(10);
     // Channel to handle data upstream messages
-    let (in_data_int, in_data_ext) = channel::<DataPacket>(10);
+    //    let (in_data_int, in_data_ext) = channel::<DataPacket>(10);
     // Internal data channels
-    let ic = InternalChannels { out_data_int, in_data_int };
+    //    let ic = InternalChannels { in_data_int };
 
     let (cmd_tx, cmd_rx) = channel::<QueuedCommand>(10);
     let commands = CommandSender { cmd_tx };
     let hal_events = hc.hal_events.clone();
 
     let notifications = EventRegistry { handlers: Arc::new(Mutex::new(HashMap::new())) };
+    let connections = LogicalConnectionsRegistry {
+        conns: Arc::new(RwLock::new(HashMap::new())),
+        sender: hc.out_data_tx.clone(),
+    };
 
-    tokio::spawn(dispatch(notifications, hc, ic, cmd_rx));
-    Nci { hal_events, commands, out_data_ext, in_data_ext }
+    tokio::spawn(dispatch(notifications, connections.clone(), hc, cmd_rx));
+    Nci { hal_events, commands, connections }
 }
 
 /// NCI module external interface
@@ -58,15 +65,8 @@ pub struct Nci {
     pub hal_events: HalEventRegistry,
     /// NCI command communication interface
     pub commands: CommandSender,
-    /// NCI outbound channel for Data messages
-    pub out_data_ext: Sender<DataPacket>,
-    /// NCI inbound channel for Data messages
-    pub in_data_ext: Receiver<DataPacket>,
-}
-
-struct InternalChannels {
-    out_data_int: Receiver<DataPacket>,
-    in_data_int: Sender<DataPacket>,
+    /// NCI logical connections
+    pub connections: LogicalConnectionsRegistry,
 }
 
 #[derive(Debug)]
@@ -82,7 +82,6 @@ struct QueuedCommand {
 }
 
 /// Sends raw commands. Only useful for facades & shims, or wrapped as a CommandSender.
-// #[derive(Clone)]
 pub struct CommandSender {
     cmd_tx: Sender<QueuedCommand>,
 }
@@ -132,6 +131,168 @@ impl Drop for CommandSender {
     }
 }
 
+/// Parameters of a logical connection
+struct ConnectionParameters {
+    callback: Option<fn(u8, u16, &[u8])>,
+    max_payload_size: u8,
+    nfcc_credits_avail: u8,
+    sendq: VecDeque<DataPacket>,
+    recvq: VecDeque<DataPacket>,
+}
+
+impl ConnectionParameters {
+    /// Flush TX queue
+    fn flush_tx(&mut self) {
+        self.sendq.clear();
+    }
+}
+
+/// To keep track of currentry open logical connections
+#[derive(Clone)]
+pub struct LogicalConnectionsRegistry {
+    conns: Arc<RwLock<HashMap<u8, Mutex<ConnectionParameters>>>>,
+    sender: UnboundedSender<DataPacket>,
+}
+
+impl LogicalConnectionsRegistry {
+    /// Create a logical connection
+    pub async fn open(
+        &mut self,
+        conn_id: u8,
+        cb: Option<fn(u8, u16, &[u8])>,
+        max_payload_size: u8,
+        nfcc_credits_avail: u8,
+    ) {
+        let conn_params = ConnectionParameters {
+            callback: cb,
+            max_payload_size,
+            nfcc_credits_avail,
+            sendq: VecDeque::<DataPacket>::new(),
+            recvq: VecDeque::<DataPacket>::new(),
+        };
+        assert!(
+            self.conns.write().await.insert(conn_id, Mutex::new(conn_params)).is_none(),
+            "A logical connection with id {:?} already exists",
+            conn_id
+        );
+    }
+    /// Set static callback
+    pub async fn set_static_callback(&mut self, conn_id: u8, cb: Option<fn(u8, u16, &[u8])>) {
+        if conn_id < 2 && cb.is_some() {
+            // Static connections
+            if let Some(conn_params) = self.conns.read().await.get(&conn_id) {
+                let mut conn_params = conn_params.lock().unwrap();
+                conn_params.callback = cb;
+            }
+        }
+    }
+    /// Close a logical connection
+    pub async fn close(&mut self, conn_id: u8) -> Option<fn(u8, u16, &[u8])> {
+        if let Some(conn_params) = self.conns.write().await.remove(&conn_id) {
+            conn_params.lock().unwrap().callback
+        } else {
+            None
+        }
+    }
+    /// Add credits to a logical connection
+    pub async fn add_credits(&self, conn_id: u8, ncreds: u8) {
+        if let Some(conn_params) = self.conns.read().await.get(&conn_id) {
+            let mut conn_params = conn_params.lock().unwrap();
+            conn_params.nfcc_credits_avail += ncreds;
+            while !conn_params.sendq.is_empty() && conn_params.nfcc_credits_avail > 0 {
+                self.sender.send(conn_params.sendq.pop_front().unwrap()).unwrap();
+                conn_params.nfcc_credits_avail -= 1;
+            }
+        }
+    }
+
+    /// Send a packet to a logical channel, splitting it if needed
+    pub async fn send_packet(&mut self, conn_id: u8, pkt: DataPacket) {
+        if let Some(conn_params) = self.conns.read().await.get(&conn_id) {
+            let mut conn_params = conn_params.lock().unwrap();
+            if let Payload(mut p) = pkt.specialize() {
+                if p.len() > conn_params.max_payload_size.into() {
+                    let conn_id = pkt.get_conn_id();
+                    while p.len() > conn_params.max_payload_size.into() {
+                        let part = DataBuilder {
+                            conn_id,
+                            pbf: PacketBoundaryFlag::Incomplete,
+                            cr: 0,
+                            payload: Some(p.split_to(conn_params.max_payload_size.into())),
+                        }
+                        .build();
+                        conn_params.sendq.push_back(part);
+                    }
+                    if !p.is_empty() {
+                        let end = DataBuilder {
+                            conn_id,
+                            pbf: PacketBoundaryFlag::CompleteOrFinal,
+                            cr: 0,
+                            payload: Some(p),
+                        }
+                        .build();
+                        conn_params.sendq.push_back(end);
+                    }
+                } else {
+                    conn_params.sendq.push_back(pkt);
+                }
+            }
+            while conn_params.nfcc_credits_avail > 0 && !conn_params.sendq.is_empty() {
+                self.sender.send(conn_params.sendq.pop_front().unwrap()).unwrap();
+                conn_params.nfcc_credits_avail -= 1;
+            }
+        }
+    }
+
+    /// Send data packet callback to the upper layers
+    pub async fn send_callback(&self, pkt: DataPacket) {
+        let conn_id = pkt.get_conn_id();
+        let ncreds = pkt.get_cr();
+        if ncreds > 0 {
+            self.add_credits(conn_id, ncreds).await;
+        }
+        let done = pkt.get_pbf() == PacketBoundaryFlag::CompleteOrFinal;
+        if let Some(conn_params) = self.conns.read().await.get(&conn_id) {
+            let mut conn_params = conn_params.lock().unwrap();
+            if !done && conn_params.recvq.is_empty() {
+                const NFC_DATA_START_CEVT: u16 = 5;
+                let cb = conn_params.callback.unwrap();
+                cb(conn_id, NFC_DATA_START_CEVT, &[]);
+            }
+            conn_params.recvq.push_back(pkt);
+            if done {
+                const NFC_DATA_CEVT_SIZE: usize = 4; // 3 for header and 1 for status
+                let cap = conn_params.recvq.len() * conn_params.max_payload_size as usize
+                    + NFC_DATA_CEVT_SIZE;
+                let mut buffer = BytesMut::with_capacity(cap);
+                buffer.put_u8(0u8); // status
+                let pkt = conn_params.recvq.pop_front().unwrap();
+                buffer.put(pkt.to_bytes());
+                while !conn_params.recvq.is_empty() {
+                    let pkt = conn_params.recvq.pop_front().unwrap();
+                    if let Payload(p) = pkt.specialize() {
+                        buffer.put(p);
+                    }
+                }
+                let data_cevt = buffer.freeze();
+                let cb = conn_params.callback.unwrap();
+                const NFC_DATA_CEVT: u16 = 3;
+                cb(conn_id, NFC_DATA_CEVT, data_cevt.as_ref());
+            }
+        }
+    }
+
+    /// Flush outgoing data queue
+    pub async fn flush_data(&mut self, conn_id: u8) -> bool {
+        if let Some(conn_params) = self.conns.read().await.get(&conn_id) {
+            conn_params.lock().unwrap().flush_tx();
+            true
+        } else {
+            false
+        }
+    }
+}
+
 /// Provides ability to register and unregister for NCI notifications
 #[derive(Clone)]
 pub struct EventRegistry {
@@ -142,7 +303,7 @@ impl EventRegistry {
     /// Indicate interest in specific NCI notification
     pub async fn register(&mut self, code: Opcode, sender: oneshot::Sender<NotificationPacket>) {
         assert!(
-            self.handlers.lock().await.insert(code, sender).is_none(),
+            self.handlers.lock().unwrap().insert(code, sender).is_none(),
             "A handler for {:?} is already registered",
             code
         );
@@ -153,14 +314,15 @@ impl EventRegistry {
         &mut self,
         code: Opcode,
     ) -> Option<oneshot::Sender<NotificationPacket>> {
-        self.handlers.lock().await.remove(&code)
+        self.handlers.lock().unwrap().remove(&code)
     }
 }
 
 async fn dispatch(
     mut ntfs: EventRegistry,
+    lcons: LogicalConnectionsRegistry,
     mut hc: Hal,
-    mut ic: InternalChannels,
+    //    ic: InternalChannels,
     mut cmd_rx: Receiver<QueuedCommand>,
 ) -> Result<()> {
     let mut pending: Option<PendingCommand> = None;
@@ -185,18 +347,28 @@ async fn dispatch(
                             Some(PendingCommand{cmd, ..}) => panic!("Waiting for {}, got {}", cmd.get_op(), this_opcode),
                             None => panic!("Unexpected status event with opcode {}", this_opcode),
                         }
-                    }
+                    },
                     Notification(ntfy) => {
-                        let code = ntfy.get_cmd_op();
-                        match ntfs.unregister(code).await {
-                            Some(sender) => {
-                                if let Err(e) = sender.send(ntfy) {
-                                    error!("notification channel closed {:?}", e);
+                        match ntfy.specialize() {
+                            ConnCreditsNotification(ccnp) => {
+                                let conns = ccnp.get_conns();
+                                for conn in conns {
+                                    lcons.add_credits(conn.conn_id, conn.ncredits).await;
                                 }
                             },
-                            None => panic!("Unhandled notification {:?}", code),
+                            _ => {
+                                let code = ntfy.get_cmd_op();
+                                match ntfs.unregister(code).await {
+                                    Some(sender) => {
+                                        if let Err(e) = sender.send(ntfy) {
+                                            error!("notification channel closed {:?}", e);
+                                        }
+                                    },
+                                    None => panic!("Unhandled notification {:?}", code),
+                                }
+                            },
                         }
-                    }
+                    },
                     _ => error!("Unexpected NCI data received {:?}", cmd),
                 }
             },
@@ -218,8 +390,7 @@ async fn dispatch(
                 timeout.as_mut().reset(max_deadline);
                 pending = None;
             },
-            Some(data) = hc.in_data_rx.recv() => ic.in_data_int.send(data).await.unwrap(),
-            Some(data) = ic.out_data_int.recv() => hc.out_data_tx.send(data).unwrap(),
+            Some(data) = hc.in_data_rx.recv() => lcons.send_callback(data).await,
             else => {
                 debug!("Select is done");
                 break;

AOSPA/android_packages_modules_Nfc

libnfc-nci/src/rust/nci/nci.rs
@@ -16,14 +16,20 @@
 //! Supports sending NCI commands to the HAL and receiving
 //! NCI messages back
 
+use bytes::{BufMut, BytesMut};
 use log::{debug, error};
 use nfc_hal::{Hal, HalEventRegistry};
-use nfc_packets::nci::{Command, DataPacket, NciPacketChild, Notification, Opcode, Response};
+use nfc_packets::nci::DataPacketChild::Payload;
+use nfc_packets::nci::NciPacketChild;
+use nfc_packets::nci::NotificationChild::ConnCreditsNotification;
+use nfc_packets::nci::{Command, DataPacket, DataPacketBuilder, Notification};
+use nfc_packets::nci::{Opcode, Packet, PacketBoundaryFlag, Response};
 use std::collections::HashMap;
-use std::sync::Arc;
+use std::collections::VecDeque;
+use std::sync::{Arc, Mutex};
 use tokio::select;
-use tokio::sync::mpsc::{channel, Receiver, Sender};
-use tokio::sync::{oneshot, Mutex};
+use tokio::sync::mpsc::{channel, Receiver, Sender, UnboundedSender};
+use tokio::sync::{oneshot, RwLock};
 use tokio::time::{sleep, Duration, Instant};
 
 pub mod api;
@@ -34,21 +40,23 @@ type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>
 /// Initialize the module and connect the channels
 pub async fn init() -> Nci {
     let hc = nfc_hal::init().await;
-    // Channel to handle data downstream messages
-    let (out_data_ext, out_data_int) = channel::<DataPacket>(10);
     // Channel to handle data upstream messages
-    let (in_data_int, in_data_ext) = channel::<DataPacket>(10);
+    //    let (in_data_int, in_data_ext) = channel::<DataPacket>(10);
     // Internal data channels
-    let ic = InternalChannels { out_data_int, in_data_int };
+    //    let ic = InternalChannels { in_data_int };
 
     let (cmd_tx, cmd_rx) = channel::<QueuedCommand>(10);
     let commands = CommandSender { cmd_tx };
     let hal_events = hc.hal_events.clone();
 
     let notifications = EventRegistry { handlers: Arc::new(Mutex::new(HashMap::new())) };
+    let connections = LogicalConnectionsRegistry {
+        conns: Arc::new(RwLock::new(HashMap::new())),
+        sender: hc.out_data_tx.clone(),
+    };
 
-    tokio::spawn(dispatch(notifications, hc, ic, cmd_rx));
-    Nci { hal_events, commands, out_data_ext, in_data_ext }
+    tokio::spawn(dispatch(notifications, connections.clone(), hc, cmd_rx));
+    Nci { hal_events, commands, connections }
 }
 
 /// NCI module external interface
@@ -57,15 +65,8 @@ pub struct Nci {
     pub hal_events: HalEventRegistry,
     /// NCI command communication interface
     pub commands: CommandSender,
-    /// NCI outbound channel for Data messages
-    pub out_data_ext: Sender<DataPacket>,
-    /// NCI inbound channel for Data messages
-    pub in_data_ext: Receiver<DataPacket>,
-}
-
-struct InternalChannels {
-    out_data_int: Receiver<DataPacket>,
-    in_data_int: Sender<DataPacket>,
+    /// NCI logical connections
+    pub connections: LogicalConnectionsRegistry,
 }
 
 #[derive(Debug)]
@@ -81,7 +82,6 @@ struct QueuedCommand {
 }
 
 /// Sends raw commands. Only useful for facades & shims, or wrapped as a CommandSender.
-// #[derive(Clone)]
 pub struct CommandSender {
     cmd_tx: Sender<QueuedCommand>,
 }
@@ -128,6 +128,168 @@ impl Drop for CommandSender {
     }
 }
 
+/// Parameters of a logical connection
+struct ConnectionParameters {
+    callback: Option<fn(u8, u16, &[u8])>,
+    max_payload_size: u8,
+    nfcc_credits_avail: u8,
+    sendq: VecDeque<DataPacket>,
+    recvq: VecDeque<DataPacket>,
+}
+
+impl ConnectionParameters {
+    /// Flush TX queue
+    fn flush_tx(&mut self) {
+        self.sendq.clear();
+    }
+}
+
+/// To keep track of currentry open logical connections
+#[derive(Clone)]
+pub struct LogicalConnectionsRegistry {
+    conns: Arc<RwLock<HashMap<u8, Mutex<ConnectionParameters>>>>,
+    sender: UnboundedSender<DataPacket>,
+}
+
+impl LogicalConnectionsRegistry {
+    /// Create a logical connection
+    pub async fn open(
+        &mut self,
+        conn_id: u8,
+        cb: Option<fn(u8, u16, &[u8])>,
+        max_payload_size: u8,
+        nfcc_credits_avail: u8,
+    ) {
+        let conn_params = ConnectionParameters {
+            callback: cb,
+            max_payload_size,
+            nfcc_credits_avail,
+            sendq: VecDeque::<DataPacket>::new(),
+            recvq: VecDeque::<DataPacket>::new(),
+        };
+        assert!(
+            self.conns.write().await.insert(conn_id, Mutex::new(conn_params)).is_none(),
+            "A logical connection with id {:?} already exists",
+            conn_id
+        );
+    }
+    /// Set static callback
+    pub async fn set_static_callback(&mut self, conn_id: u8, cb: Option<fn(u8, u16, &[u8])>) {
+        if conn_id < 2 && cb.is_some() {
+            // Static connections
+            if let Some(conn_params) = self.conns.read().await.get(&conn_id) {
+                let mut conn_params = conn_params.lock().unwrap();
+                conn_params.callback = cb;
+            }
+        }
+    }
+    /// Close a logical connection
+    pub async fn close(&mut self, conn_id: u8) -> Option<fn(u8, u16, &[u8])> {
+        if let Some(conn_params) = self.conns.write().await.remove(&conn_id) {
+            conn_params.lock().unwrap().callback
+        } else {
+            None
+        }
+    }
+    /// Add credits to a logical connection
+    pub async fn add_credits(&self, conn_id: u8, ncreds: u8) {
+        if let Some(conn_params) = self.conns.read().await.get(&conn_id) {
+            let mut conn_params = conn_params.lock().unwrap();
+            conn_params.nfcc_credits_avail += ncreds;
+            while !conn_params.sendq.is_empty() && conn_params.nfcc_credits_avail > 0 {
+                self.sender.send(conn_params.sendq.pop_front().unwrap()).unwrap();
+                conn_params.nfcc_credits_avail -= 1;
+            }
+        }
+    }
+
+    /// Send a packet to a logical channel, splitting it if needed
+    pub async fn send_packet(&mut self, conn_id: u8, pkt: DataPacket) {
+        if let Some(conn_params) = self.conns.read().await.get(&conn_id) {
+            let mut conn_params = conn_params.lock().unwrap();
+            if let Payload(mut p) = pkt.specialize() {
+                if p.len() > conn_params.max_payload_size.into() {
+                    let conn_id = pkt.get_conn_id();
+                    while p.len() > conn_params.max_payload_size.into() {
+                        let part = DataPacketBuilder {
+                            conn_id,
+                            pbf: PacketBoundaryFlag::Incomplete,
+                            cr: 0,
+                            payload: Some(p.split_to(conn_params.max_payload_size.into())),
+                        }
+                        .build();
+                        conn_params.sendq.push_back(part);
+                    }
+                    if !p.is_empty() {
+                        let end = DataPacketBuilder {
+                            conn_id,
+                            pbf: PacketBoundaryFlag::CompleteOrFinal,
+                            cr: 0,
+                            payload: Some(p),
+                        }
+                        .build();
+                        conn_params.sendq.push_back(end);
+                    }
+                } else {
+                    conn_params.sendq.push_back(pkt);
+                }
+            }
+            while conn_params.nfcc_credits_avail > 0 && !conn_params.sendq.is_empty() {
+                self.sender.send(conn_params.sendq.pop_front().unwrap()).unwrap();
+                conn_params.nfcc_credits_avail -= 1;
+            }
+        }
+    }
+
+    /// Send data packet callback to the upper layers
+    pub async fn send_callback(&self, pkt: DataPacket) {
+        let conn_id = pkt.get_conn_id();
+        let ncreds = pkt.get_cr();
+        if ncreds > 0 {
+            self.add_credits(conn_id, ncreds).await;
+        }
+        let done = pkt.get_pbf() == PacketBoundaryFlag::CompleteOrFinal;
+        if let Some(conn_params) = self.conns.read().await.get(&conn_id) {
+            let mut conn_params = conn_params.lock().unwrap();
+            if !done && conn_params.recvq.is_empty() {
+                const NFC_DATA_START_CEVT: u16 = 5;
+                let cb = conn_params.callback.unwrap();
+                cb(conn_id, NFC_DATA_START_CEVT, &[]);
+            }
+            conn_params.recvq.push_back(pkt);
+            if done {
+                const NFC_DATA_CEVT_SIZE: usize = 4; // 3 for header and 1 for status
+                let cap = conn_params.recvq.len() * conn_params.max_payload_size as usize
+                    + NFC_DATA_CEVT_SIZE;
+                let mut buffer = BytesMut::with_capacity(cap);
+                buffer.put_u8(0u8); // status
+                let pkt = conn_params.recvq.pop_front().unwrap();
+                buffer.put(pkt.to_bytes());
+                while !conn_params.recvq.is_empty() {
+                    let pkt = conn_params.recvq.pop_front().unwrap();
+                    if let Payload(p) = pkt.specialize() {
+                        buffer.put(p);
+                    }
+                }
+                let data_cevt = buffer.freeze();
+                let cb = conn_params.callback.unwrap();
+                const NFC_DATA_CEVT: u16 = 3;
+                cb(conn_id, NFC_DATA_CEVT, data_cevt.as_ref());
+            }
+        }
+    }
+
+    /// Flush outgoing data queue
+    pub async fn flush_data(&mut self, conn_id: u8) -> bool {
+        if let Some(conn_params) = self.conns.read().await.get(&conn_id) {
+            conn_params.lock().unwrap().flush_tx();
+            true
+        } else {
+            false
+        }
+    }
+}
+
 /// Provides ability to register and unregister for NCI notifications
 #[derive(Clone)]
 pub struct EventRegistry {
@@ -138,7 +300,7 @@ impl EventRegistry {
     /// Indicate interest in specific NCI notification
     pub async fn register(&mut self, code: Opcode, sender: oneshot::Sender<Notification>) {
         assert!(
-            self.handlers.lock().await.insert(code, sender).is_none(),
+            self.handlers.lock().unwrap().insert(code, sender).is_none(),
             "A handler for {:?} is already registered",
             code
         );
@@ -146,14 +308,15 @@ impl EventRegistry {
 
     /// Remove interest in specific NCI notification
     pub async fn unregister(&mut self, code: Opcode) -> Option<oneshot::Sender<Notification>> {
-        self.handlers.lock().await.remove(&code)
+        self.handlers.lock().unwrap().remove(&code)
     }
 }
 
 async fn dispatch(
     mut ntfs: EventRegistry,
+    lcons: LogicalConnectionsRegistry,
     mut hc: Hal,
-    mut ic: InternalChannels,
+    //    ic: InternalChannels,
     mut cmd_rx: Receiver<QueuedCommand>,
 ) -> Result<()> {
     let mut pending: Option<PendingCommand> = None;
@@ -178,18 +341,28 @@ async fn dispatch(
                             Some(PendingCommand{cmd, ..}) => panic!("Waiting for {:?}, got {:?}", cmd.get_op(), this_opcode),
                             None => panic!("Unexpected status event with opcode {:?}", this_opcode),
                         }
-                    }
+                    },
                     NciPacketChild::Notification(ntfy) => {
-                        let code = ntfy.get_cmd_op();
-                        match ntfs.unregister(code).await {
-                            Some(sender) => {
-                                if let Err(e) = sender.send(ntfy) {
-                                    error!("notification channel closed {:?}", e);
+                        match ntfy.specialize() {
+                            ConnCreditsNotification(ccnp) => {
+                                let conns = ccnp.get_conns();
+                                for conn in conns {
+                                    lcons.add_credits(conn.conn_id, conn.ncredits).await;
                                 }
                             },
-                            None => panic!("Unhandled notification {:?}", code),
+                            _ => {
+                                let code = ntfy.get_cmd_op();
+                                match ntfs.unregister(code).await {
+                                    Some(sender) => {
+                                        if let Err(e) = sender.send(ntfy) {
+                                            error!("notification channel closed {:?}", e);
+                                        }
+                                    },
+                                    None => panic!("Unhandled notification {:?}", code),
+                                }
+                            },
                         }
-                    }
+                    },
                     _ => error!("Unexpected NCI data received {:?}", cmd),
                 }
             },
@@ -211,8 +384,7 @@ async fn dispatch(
                 timeout.as_mut().reset(max_deadline);
                 pending = None;
             },
-            Some(data) = hc.in_data_rx.recv() => ic.in_data_int.send(data).await.unwrap(),
-            Some(data) = ic.out_data_int.recv() => hc.out_data_tx.send(data).unwrap(),
+            Some(data) = hc.in_data_rx.recv() => lcons.send_callback(data).await,
             else => {
                 debug!("Select is done");
                 break;