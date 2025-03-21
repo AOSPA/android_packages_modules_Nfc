e7e67d5cfe698e959ff04310bfc4880c21613dfd
lufucheng <lufucheng@xiaomi.corp-partner.google.com>
2025-03-21 12:53:24 +0800

    [nfc] Process it (NFC Close State Machine) into an independent method for easy calling and protect mDeviceHost.commitRouting()
    1.mState == NfcAdapter.STATE_OFF || mState == NfcAdapter.STATE_TURNING_OFF can be processed into an independent mode for easy use
    2.mDeviceHost.commitRouting() is protected by the NFC switch state machine in Handelr and other places. The API interface open to the outside should be protected even more.
    
    Bug: 405256433
    Test: Manual
    
    Change-Id: Ib3e83eebdcced9d9fa28ef065866838173717c30
    Signed-off-by: lufucheng <lufucheng@xiaomi.corp-partner.google.com>

--- NfcNci/src/com/android/nfc/NfcService.java
+++ NfcNci/src/com/android/nfc/NfcService.java
@@ -3571,6 +3571,11 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
 
         @Override
         public int commitRouting() throws RemoteException {
+            if (isNfcDisabledOrDisabling()) {
+                Log.d(TAG, "Skip commit routing when NFCC is off "
+                        + "or turning off");
+                return STATUS_UNKNOWN_ERROR;
+            }
             if (DBG) Log.i(TAG, "commitRouting");
             NfcPermissions.enforceAdminPermissions(mContext);
             return mDeviceHost.commitRouting();
@@ -4283,6 +4288,12 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
         }
     }
 
+    boolean isNfcDisabledOrDisabling() {
+        synchronized (this) {
+            return (mState == NfcAdapter.STATE_OFF || mState == NfcAdapter.STATE_TURNING_OFF);
+        }
+    }
+
     boolean isNfcEnabled() {
         synchronized (this) {
             return mState == NfcAdapter.STATE_ON;
@@ -4924,8 +4935,7 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
                 case MSG_COMMIT_ROUTING: {
                     Log.d(TAG, "handleMessage: MSG_COMMIT_ROUTING");
                     synchronized (NfcService.this) {
-                        if (mState == NfcAdapter.STATE_OFF
-                                || mState == NfcAdapter.STATE_TURNING_OFF) {
+                        if (isNfcDisabledOrDisabling()) {
                             Log.d(TAG, "handleMessage: Skip commit routing when NFCC is off "
                                     + "or turning off");
                             if (mCommitRoutingCountDownLatch != null) {
